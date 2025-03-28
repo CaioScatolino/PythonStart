a5afa2a1ef9aec08756faa74e3d32a9fcb988816
Caio Scatolino <caio.scatolino@outlook.com>
2025-03-28 11:34:09 -0300

    pivot_table

--- pivot_table.py
+++ pivot_table.py
@@ -1,3 +1,21 @@
 import pandas as pd
 
 data = pd.read_excel('./data/vendaCarros.xlsx')
+
+# print(data)
+
+df = data[['Fabricante', 'ValorVenda', 'Ano']]
+print(df)
+
+# Criando a tabela Pivot
+
+pivot_table = df.pivot_table(
+    index='Ano',
+    columns='Fabricante',
+    values='ValorVenda',
+    aggfunc='sum'
+)
+
+print(pivot_table)
+
+pivot_table.to_excel('./data/pivot_table.xlsx', 'Relatorio')
